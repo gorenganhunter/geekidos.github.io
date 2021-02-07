a75c24c3158cbf5744e4f2ec2589c24d195cfd2c
GEEKiDoS <geek_ds@foxmail.com>
2021-02-07 15:31:28 +0800

    add d4dj editor

---
+++ d4dj/editor/precache-manifest.1b2363a022241c7b1f6e0be604a27f2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57fe87468bff097e0ff0a722ecf77315",
+    "url": "/ebbb/index.html"
+  },
+  {
+    "revision": "fdb35492c65d26f80433",
+    "url": "/ebbb/static/css/main.5238747f.chunk.css"
+  },
+  {
+    "revision": "b8d8dfb65ebb2421fbd3",
+    "url": "/ebbb/static/js/2.53f5d7a3.chunk.js"
+  },
+  {
+    "revision": "8b055075f934c3b14d1bd5564a7a7891",
+    "url": "/ebbb/static/js/2.53f5d7a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb35492c65d26f80433",
+    "url": "/ebbb/static/js/main.13736ae3.chunk.js"
+  },
+  {
+    "revision": "db8fd8eafc20af91d694",
+    "url": "/ebbb/static/js/runtime-main.e3e15f4b.js"
+  }
+]);
