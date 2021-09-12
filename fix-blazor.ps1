a5ebb2f2f6beaade8b3af267dd3cefbd9675231c
GEEKiDoS <mnb_2001@qq.com>
2021-09-12 12:49:54 +0800

    add .nojekyll

--- fix-blazor.ps1
+++ fix-blazor.ps1
@@ -1,11 +1,3 @@
 (Get-Content .\d4dj\import\index.html) `
-    -replace '<base href="/" />', '<base href="/d4dj/import/" />' `
-    -replace '_framework', 'framework' |
+    -replace '<base href="/" />', '<base href="/d4dj/import/" />' ` |
     Out-File .\d4dj\import\index.html
-
-Rename-Item .\d4dj\import\_framework "framework"
-Rename-Item .\d4dj\import\framework\_bin "bin"
-(Get-Content .\d4dj\import\framework\blazor.webassembly.js) `
-    -replace '_framework', 'framework' `
-    -replace '_bin', 'bin' |
-    Out-File .\d4dj\import\framework\blazor.webassembly.js
